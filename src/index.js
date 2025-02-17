652699e8194d3b546766a88663fea8ca8e01fa5d
Rupam <rupammondal564@gmail.com>
2025-02-17 12:40:14 +0530

    Authentication added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import express from 'express';
 import { ConnectDb } from './Config/Dbconfig.js';
 import UserRouter from './Routes/UserRouter.js';
 import cors from 'cors';
+import { IsAuthenticated } from './Middlewares/Authmiddleware.js';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/ping' , (req , res) => {
+app.get('/ping', IsAuthenticated , (req , res) => {
     return res.json({
         success:true,
         message:"Ping"
