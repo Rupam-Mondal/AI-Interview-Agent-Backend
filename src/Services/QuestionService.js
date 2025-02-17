98466798c7d6a8d8ad1a52283bb7a063bc47a388
Rupam <rupammondal564@gmail.com>
2025-02-17 20:16:50 +0530

    Update

--- src/Services/QuestionService.js
+++ src/Services/QuestionService.js
@@ -3,8 +3,21 @@ import { GetQuestionsInterview } from "../Apis/Gptapi.js";
 export async function QuestionService({ topic, experience }){
     try {
         const response = await GetQuestionsInterview(topic , experience);
-        if(!response){
+        console.log(typeof response);
+        if (!response) {
             throw null;
+        }
+        const arr = [];
+        let k = 0;
+        let i = 0;
+        while(i < response.length){
+            if(response[i] != '/n'){
+                arr[k] += response[i];
+            }
+            else{
+                k++;
+            }
+            i++;
         }
         return response;
     } catch (error) {
