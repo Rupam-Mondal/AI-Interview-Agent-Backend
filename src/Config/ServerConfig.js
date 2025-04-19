e3f7500b314b64fc9ec9ab9e7aef7f45d68a9824
Rupam <rupammondal564@gmail.com>
2025-04-19 22:15:06 +0530

    API key updated

--- src/Config/ServerConfig.js
+++ src/Config/ServerConfig.js
@@ -5,3 +5,4 @@ dotenv.config();
 export const db_url = process.env.DB_URL;
 export const jwt_secret_key = process.env.JWT_SECRET;
 export const key = process.env.GITHUB_GPT_4O;
+export const openaiKey = process.env.RUPAM_GITHUB_4;
