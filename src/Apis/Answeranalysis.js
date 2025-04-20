26705993fbaaa997371528d9b896b892c2b62514
Rupam <rupammondal564@gmail.com>
2025-04-20 06:25:07 +0530

    GPT 3o to Gpt 40 Migrated successfully

--- src/Apis/Answeranalysis.js
+++ src/Apis/Answeranalysis.js
@@ -1,20 +1,36 @@
 import client from "../Config/OpenaiConfig.js";
+import ModelClient, { isUnexpected } from "@azure-rest/ai-inference";
+import { AzureKeyCredential } from "@azure/core-auth";
+import { openaiKey } from "../Config/ServerConfig.js";
+const token = openaiKey;
+const endpoint = "https://models.github.ai/inference";
+const model = "openai/gpt-4.1";
 
 export async function Analysis(Object) {
-    try {
-        const response = await client.chat.completions.create({
-            messages: [
-                { role: "developer", content: "" },
-                {
-                    role: "user", content: `Analyse these answer with respect to question ${JSON.stringify(Object)}
+    const client = ModelClient(endpoint, new AzureKeyCredential(token));
+        const response = await client.path("/chat/completions").post({
+            body: {
+                model,
+                temperature: 0.7,
+                top_p: 1.0,
+                messages: [
+                    {
+                        role: "system",
+                        content: "You are a helpful assistant that generates visual animation instructions for code steps."
+                    },
+                    {
+                        role: "user",
+                        content: `Analyse these answer with respect to question ${JSON.stringify(Object)}
                                         write scope of improvements for each question, and answer of each question in short, 
-                                        do not write extra sentence and also give marks out of 5, also give total marks at end, also attach the topic name at last` }
-            ],
-            model: "o3-mini"
+                                        do not write extra sentence and also give marks out of 5, also give total marks at end, also attach the topic name at last`
+                    }
+                ]
+            }
         });
-
-        return response.choices[0].message.content;
-    } catch (error) {
-        throw error;
-    }
+    
+        if (isUnexpected(response)) {
+            throw response.body.error;
+        }
+    
+        return response.body.choices[0].message.content;
 }
